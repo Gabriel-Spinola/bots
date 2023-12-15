120961fc9ba0aa75889d3700bd95acd150f607e0
Gabriel Spinola <gabrielspinola77@gmail.com>
2023-12-15 15:41:55 -0300

    client

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,10 @@
-console.log("hello, world!")
+import { DISCORD_SECRET } from './config.json'
+import Discord, { GatewayIntentBits, Events } from 'discord.js'
+
+const client = new Discord.Client({ intents: [GatewayIntentBits.Guilds] })
+
+client.on(Events.ClientReady, (client) => {
+  console.log(`Logged as ${client.user.tag}`)
+})
+
+client.login(DISCORD_SECRET)
