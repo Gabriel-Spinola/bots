de867a6e3998b90fed15ff05281ffb9479c91744
Gabriel Spinola <gabrielspinola77@gmail.com>
2023-12-15 16:04:35 -0300

    ping command

--- dist/app.js
+++ dist/app.js
@@ -1,39 +1,33 @@
 "use strict";
-var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
-    if (k2 === undefined) k2 = k;
-    var desc = Object.getOwnPropertyDescriptor(m, k);
-    if (!desc || ("get" in desc ? !m.__esModule : desc.writable || desc.configurable)) {
-      desc = { enumerable: true, get: function() { return m[k]; } };
-    }
-    Object.defineProperty(o, k2, desc);
-}) : (function(o, m, k, k2) {
-    if (k2 === undefined) k2 = k;
-    o[k2] = m[k];
-}));
-var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
-    Object.defineProperty(o, "default", { enumerable: true, value: v });
-}) : function(o, v) {
-    o["default"] = v;
-});
-var __importStar = (this && this.__importStar) || function (mod) {
-    if (mod && mod.__esModule) return mod;
-    var result = {};
-    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
-    __setModuleDefault(result, mod);
-    return result;
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 require("dotenv/config");
-const discord_js_1 = __importStar(require("discord.js"));
-const client = new discord_js_1.default.Client({ intents: [discord_js_1.GatewayIntentBits.Guilds] });
+const discord_js_1 = require("discord.js");
+const ping_1 = __importDefault(require("./ping"));
+const client = new discord_js_1.Client({ intents: [discord_js_1.GatewayIntentBits.Guilds] });
 client.on(discord_js_1.Events.ClientReady, (client) => {
     console.log(`Logged as ${client.user.tag}`);
 });
 client.on(discord_js_1.Events.InteractionCreate, async (interaction) => {
-    if (!interaction.isChatInputCommand()) {
+    if (!interaction.isChatInputCommand())
+        return;
+    if (interaction.commandName !== ping_1.default.data.name) {
         return;
     }
-    //const command = interaction.client.comm
+    try {
+        await ping_1.default.execute(interaction);
+    }
+    catch (error) {
+        console.error(error);
+        if (interaction.replied || interaction.deferred) {
+            await interaction.followUp({ content: 'There was an error while executing this command!', ephemeral: true });
+        }
+        else {
+            await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
+        }
+    }
 });
 client.login(process.env.DISCORD_SECRET);
 //# sourceMappingURL=app.js.map
--- src/app.ts
+++ src/app.ts
@@ -1,18 +1,31 @@
 import 'dotenv/config'
-import Discord, { GatewayIntentBits, Events } from 'discord.js'
+import { GatewayIntentBits, Events, Collection, Client} from 'discord.js'
+import command from './ping'
 
-const client = new Discord.Client({ intents: [GatewayIntentBits.Guilds] })
+const client = new Client({ intents: [GatewayIntentBits.Guilds] })
 
 client.on(Events.ClientReady, (client) => {
   console.log(`Logged as ${client.user.tag}`)
 })
 
 client.on(Events.InteractionCreate, async (interaction) => {
-  if (!interaction.isChatInputCommand()) {
-    return
+	if (!interaction.isChatInputCommand()) return;
+
+  if (interaction.commandName !== command.data.name) {
+    return 
   }
 
-  //const command = interaction.client.comm
+  try {
+    await command.execute(interaction)
+  } catch (error: unknown) {
+    console.error(error);
+    
+    if (interaction.replied || interaction.deferred) {
+			await interaction.followUp({ content: 'There was an error while executing this command!', ephemeral: true });
+		} else {
+			await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
+		}
+  }
 })
 
 client.login(process.env.DISCORD_SECRET)
