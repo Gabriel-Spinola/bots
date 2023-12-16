8f90a7e8a61169bebc68bc303b27dcbc0ec733fc
Gabriel Spinola <gabrielspinola77@gmail.com>
2023-12-16 08:15:54 -0300

    test

--- src/app.mjs
+++ src/app.mjs
@@ -2,48 +2,50 @@ import 'dotenv/config'
 import { GatewayIntentBits, Events, Client, REST, Routes } from 'discord.js'
 import command from './commands/ping.mjs'
 
-const client = new Client({ intents: [
-  GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages,
-  GatewayIntentBits.GuildMessageReactions, GatewayIntentBits.GuildMessageTyping, 
-  //GatewayIntentBits.MessageContent
-]})
+export default async function handler(request, response) { 
+  const client = new Client({ intents: [
+    GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.GuildMessageReactions, GatewayIntentBits.GuildMessageTyping, 
+    //GatewayIntentBits.MessageContent
+  ]})
 
-client.on(Events.ClientReady, (client) => {
-  console.log(`Logged as ${client.user.tag}`)
-  const rest = new REST().setToken(process.env.DISCORD_SECRET);
+  client.on(Events.ClientReady, (client) => {
+    console.log(`Logged as ${client.user.tag}`)
+    const rest = new REST().setToken(process.env.DISCORD_SECRET);
 
-  (async() => {
+    (async() => {
+      try {
+        console.log(`Started refreshing ${JSON.stringify(command)} application (/) commands.`);
+
+        // The put method is used to fully refresh all commands in the guild with the current set
+        const data = await rest.put(
+          Routes.applicationCommands(process.env.CLIENT_ID),
+          { body: [command.data.toJSON()] },
+        );
+
+        console.log(`Successfully reloaded ${JSON.stringify(data)} application (/) commands.`);
+      } catch (error) {
+        // And of course, make sure you catch and log any errors!
+        console.error(error);
+      }
+    })()
+  })
+
+  client.on(Events.InteractionCreate, async (interaction) => {
+    if (!interaction.isChatInputCommand()) return;
+
     try {
-      console.log(`Started refreshing ${JSON.stringify(command)} application (/) commands.`);
-
-      // The put method is used to fully refresh all commands in the guild with the current set
-      const data = await rest.put(
-        Routes.applicationCommands(process.env.CLIENT_ID),
-        { body: [command.data.toJSON()] },
-      );
-
-      console.log(`Successfully reloaded ${JSON.stringify(data)} application (/) commands.`);
+      await command.execute(interaction)
     } catch (error) {
-      // And of course, make sure you catch and log any errors!
       console.error(error);
+      
+      if (interaction.replied || interaction.deferred) {
+        await interaction.followUp({ content: 'There was an error while executing this command!', ephemeral: true });
+      } else {
+        await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
+      }
     }
-  })()
-})
+  })
 
-client.on(Events.InteractionCreate, async (interaction) => {
-  if (!interaction.isChatInputCommand()) return;
-
-  try {
-    await command.execute(interaction)
-  } catch (error) {
-    console.error(error);
-    
-    if (interaction.replied || interaction.deferred) {
-			await interaction.followUp({ content: 'There was an error while executing this command!', ephemeral: true });
-		} else {
-			await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
-		}
-  }
-})
-
-client.login(process.env.DISCORD_SECRET)
+  client.login(process.env.DISCORD_SECRET)
+}
